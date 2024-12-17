aa82ca494db687d603d46fa8d4d65bdb274042ac
Muskan Swarup <muskanswarup680@gmail.com>
2024-12-17 21:30:43 +0530

    improve UI and functionality

--- 8. Stone Paper Scissors/script.js
+++ 8. Stone Paper Scissors/script.js
@@ -7,57 +7,62 @@ let cScore = 0;
 
 const choices = document.querySelectorAll(".choice");
 
-const computerChoice = () =>{
-    const options = ["rock" , "paper", "scissors"];
-    const randomIndex =  Math.floor(Math.random() * 3);   
+const computerChoice = () => {
+    const options = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
     return options[randomIndex];
-}
+};
 
-const drawGame = ()=>{
-    console.log("Game was draw");
-}
+const drawGame = (userChoice, compChoice) => {
+    console.log("Game was a draw");
+    displayBoard.innerText = `It's a draw! You both chose ${userChoice}.`;
+    displayBoard.style.backgroundColor = "gray";
+    displayBoard.style.color = "white";
+};
 
-const showWinner = (userWin) =>{
-    if(userWin){
+const showWinner = (userWin, userChoice, compChoice) => {
+    if (userWin) {
         uScore++;
         console.log("You won!");
-        displayBoard.innerText = `You win!`;
+        displayBoard.innerText = `You win! You chose ${userChoice}, Computer chose ${compChoice}.`;
         displayBoard.style.backgroundColor = "green";
         displayBoard.style.color = "blanchedalmond";
-
+        displayBoard.style.fontSize = "1rem";
 
         userScore.innerText = uScore;
-    }else{
+    } else {
         cScore++;
         console.log("You lost!");
-        displayBoard.innerText = "You lose!";
-        displayBoard.style.backgroundColor = "red";
+        displayBoard.innerText = `You lose! You chose ${userChoice}, Computer chose ${compChoice}.`;
+        displayBoard.style.backgroundColor = "#CA054D";
         displayBoard.style.color = "blanchedalmond";
+        displayBoard.style.fontSize = "1rem";
+
+
         compScore.innerText = cScore;
     }
-}
+};
 
-const playGames = (userChoice) =>{
+const playGames = (userChoice) => {
     console.log("User choice:", userChoice);
     const compChoice = computerChoice();
-    console.log("Computer choice :" , compChoice)
+    console.log("Computer choice:", compChoice);
 
-    if(userChoice == compChoice){
-        drawGame();
-    }else{
+    if (userChoice === compChoice) {
+        drawGame(userChoice, compChoice);
+    } else {
         let userWin = true;
 
-        if(userChoice == "rock"){
+        if (userChoice === "rock") {
             userWin = compChoice === "paper" ? false : true;
-        }else if(userChoice == "paper"){
+        } else if (userChoice === "paper") {
             userWin = compChoice === "scissors" ? false : true;
-        }else{
+        } else {
             userWin = compChoice === "rock" ? false : true;
         }
-        showWinner(userWin);
+        showWinner(userWin, userChoice, compChoice);
     }
-    
-}
+};
 
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
